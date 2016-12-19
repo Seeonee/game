44ed4a96dd818cd42ae13a28404e6ab08d709e98
Seeonee <gecko4D@yahoo.com>
2016-12-18 19:21:24 -0500

    tsquare_bitmap_cache_and_cascade_up
    Since all tsquares use the same simple bitmap, they now cache and reuse
    it after the first square does the heavy lifting. Also, squares now
    cascade up because it looks cooler.

--- js/gfx/TSquare.js
+++ js/gfx/TSquare.js
@@ -1,13 +1,16 @@
 // Falling squares for the (T)itle menu screen.
 var TSquare = function(game) {
     this.game = game;
-    var d = TSquare.D;
-    var bitmap = this.game.add.bitmapData(d, d);
-    var c = bitmap.context;
-    c.fillStyle = this.game.settings.colors.WHITE.s;
-    c.beginPath();
-    c.fillRect(0, 0, d, d);
-    Phaser.Sprite.call(this, game, 0, 0, bitmap);
+    if (TSquare.BITMAP == undefined) {
+        var d = TSquare.D;
+        var bitmap = this.game.add.bitmapData(d, d);
+        var c = bitmap.context;
+        c.fillStyle = this.game.settings.colors.WHITE.s;
+        c.beginPath();
+        c.fillRect(0, 0, d, d);
+        TSquare.BITMAP = bitmap;
+    }
+    Phaser.Sprite.call(this, game, 0, 0, TSquare.BITMAP);
     this.anchor.setTo(0.5, 0.5);
     this.visible = false;
 
@@ -15,8 +18,13 @@ var TSquare = function(game) {
     this.scalerange = 1 - this.scalemin;
     this.xmin = this.game.camera.x + TSquare.XBOUNDS;
     this.xrange = this.game.camera.width - (2 * TSquare.XBOUNDS);
-    this.y0 = this.game.camera.y;
-    this.y1 = this.y0 + this.game.camera.height;
+    if (TSquare.CASCADE_UP) {
+        this.y0 = this.game.camera.y + this.game.camera.height;
+        this.y1 = this.game.camera.y;
+    } else {
+        this.y0 = this.game.camera.y;
+        this.y1 = this.game.camera.y + this.game.camera.height;
+    }
 };
 
 TSquare.prototype = Object.create(Phaser.Sprite.prototype);
@@ -30,14 +38,17 @@ TSquare.YPAD_RATIO = 1.5;
 TSquare.XBOUNDS = 80;
 TSquare.MIN_SCALE = 0.1;
 TSquare.ROTATION = 0.6 * Math.PI; // 3 * Math.PI;
+TSquare.BITMAP = undefined;
+TSquare.CASCADE_UP = true;
 
 
-// Horizontal flash at our position.
+// Let the square trickle up.
 TSquare.prototype.cascade = function(zgroup, tint) {
     var scale = this.scalemin + Math.random() * this.scalerange;
     this.x = this.xmin + Math.random() * this.xrange;
     var dy = TSquare.D * scale * TSquare.YPAD_RATIO;
-    this.y = this.y0 - dy;
+    var sign = TSquare.CASCADE_UP ? 1 : -1;
+    this.y = this.y0 + sign * dy;
     this.scale.setTo(scale);
     // this.rotation = Math.random() * 2 * Math.PI;
     var dr = (Math.random() - 0.5) * TSquare.ROTATION;
@@ -48,7 +59,7 @@ TSquare.prototype.cascade = function(zgroup, tint) {
     var time = TSquare.LIFESPAN - TSquare.VARIANCE +
         Math.random() * 2 * TSquare.VARIANCE;
     var t = this.game.add.tween(this);
-    t.to({ y: this.y1 + dy, rotation: this.rotation + dr },
+    t.to({ y: this.y1 - sign * dy, rotation: this.rotation + dr },
         time, Phaser.Easing.Linear.InOut, true);
     t.onComplete.add(function() {
         this.kill();
