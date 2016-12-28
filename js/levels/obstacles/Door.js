adab54c2c18bfe5e85e2d4f032251febc1fe8514
Seeonee <gecko4D@yahoo.com>
2016-12-28 13:21:11 -0500

    door_colors_and_bitmap_caching
    Tweaked the colors and added bitmap caching.

--- js/levels/obstacles/Door.js
+++ js/levels/obstacles/Door.js
@@ -1,22 +1,29 @@
 // A door that requires a key to pass through.
 var DoorSprite = function(game, x, y, name, palette) {
     this.game = game;
-    var d = DoorSprite.D;
-    var bitmap = this.game.add.bitmapData(d, d);
-    var c = bitmap.context;
-    c.fillStyle = this.game.settings.colors.WHITE.s;
-    c.fillRect(0, 0, d, d);
-    Phaser.Sprite.call(this, game, x, y, bitmap);
+    if (DoorSprite.CACHED_BITMAP1 == undefined) {
+        var d = DoorSprite.D;
+        var bitmap = this.game.add.bitmapData(d, d);
+        var c = bitmap.context;
+        c.fillStyle = this.game.settings.colors.WHITE.s;
+        c.fillRect(0, 0, d, d);
+        DoorSprite.CACHED_BITMAP1 = bitmap;
+    }
+    Phaser.Sprite.call(this, game, x, y, DoorSprite.CACHED_BITMAP1);
     this.anchor.setTo(0.5);
     this.rotation = Math.PI / 4;
 
     // More decals.
-    var d = DoorSprite.D2;
-    var bitmap = this.game.add.bitmapData(d, d);
-    var c = bitmap.context;
-    c.fillStyle = this.game.settings.colors.WHITE.s;
-    c.fillRect(0, 0, d, d);
-    this.inner = this.addChild(this.game.add.sprite(0, 0, bitmap));
+    if (DoorSprite.CACHED_BITMAP2 == undefined) {
+        var d = DoorSprite.D2;
+        var bitmap = this.game.add.bitmapData(d, d);
+        var c = bitmap.context;
+        c.fillStyle = this.game.settings.colors.WHITE.s;
+        c.fillRect(0, 0, d, d);
+        DoorSprite.CACHED_BITMAP2 = bitmap;
+    }
+    this.inner = this.addChild(this.game.add.sprite(
+        0, 0, DoorSprite.CACHED_BITMAP2));
     this.inner.anchor.setTo(0.5);
 
     // And our icon!
@@ -41,8 +48,9 @@ DoorSprite.UNLOCK_SCALE = 2;
 
 // Update colors.
 DoorSprite.prototype.setPalette = function(palette) {
-    this.tint = palette.c2.i;
-    this.inner.tint = palette.c1.i;
+    this.tint = palette.c1.i;
+    this.inner.tint = palette.c3.i;
+    this.icon.tint = palette.c2.i;
 };
 
 // Call to destruct!
