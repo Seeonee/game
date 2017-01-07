6ae4579e6e15b60d7b69d8a9518d1f34ca0f52ca
Seeonee <gecko4D@yahoo.com>
2017-01-06 22:16:39 -0500

    fireball_foe
    Added support for fixed rotation fireball enemies.

---
+++ js/gfx/obstacle/foe/FireballSprite.js
@@ -0,0 +1,151 @@
+// An orbiting fireball trap.
+var FireballSprite = function(fireball, x, y, palette) {
+    this.fireball = fireball;
+    Phaser.Sprite.call(this, fireball.game, x, y);
+    this.anchor.setTo(0.5);
+
+    this.spinner = this.addChild(this.game.add.sprite(0, 0));
+    this.spinner.rotation = this.fireball.startAngle;
+    this.tween = this.game.add.tween(this.spinner);
+    this.tween.to({ rotation: fireball.rotation }, fireball.time,
+        Phaser.Easing.Linear.None, true, 0,
+        Number.POSITIVE_INFINITY, fireball.yoyo);
+
+    this.bitmap = this.game.bitmapCache.get(
+        FireballSprite.painter);
+    this.flame = this.spinner.addChild(this.game.add.sprite(
+        0, this.fireball.radius, this.bitmap));
+    this.flame.anchor.setTo(0.5);
+    this.flame.scale.setTo(FireballSprite.LEAD_FLAME_SCALE);
+
+    this.orb = this.spinner.addChild(this.game.add.sprite(
+        0, this.fireball.radius));
+    this.orb.anchor.setTo(0.5);
+    this.setPalette(palette);
+
+    this.orb.rotation = -this.fireball.startAngle;
+    this.tween2 = this.game.add.tween(this.orb);
+    this.tween2.to({ rotation: -fireball.rotation }, fireball.time,
+        Phaser.Easing.Linear.None, true, 0,
+        Number.POSITIVE_INFINITY, fireball.yoyo);
+
+    this.hitbox = fireball.hitbox;
+    this.hitbox.x = 0;
+    this.hitbox.y = 0;
+    this.orb.addChild(this.hitbox);
+
+    this.sparkTime = -1;
+};
+
+FireballSprite.prototype = Object.create(Phaser.Sprite.prototype);
+FireballSprite.prototype.constructor = FireballSprite;
+
+// Constants.
+FireballSprite.LEAD_FLAME_SCALE = 0.8;
+FireballSprite.SPARK_R = 24;
+FireballSprite.SPARK_INTERVAL = 100; // ms
+FireballSprite.SPARK_LIFETIME = 700; // ms
+FireballSprite.SPARK_STARTING_SCALE = 0.7;
+FireballSprite.SPARK_TINT_DELAY = 50; // ms
+FireballSprite.SPARK_GROWTH_TIME = 100; // ms
+FireballSprite.SPARK_GROWTH_SCALE = 1.05;
+FireballSprite.DRIFT_VARIANCE = 17;
+FireballSprite.ANGLE_VARIANCE = Math.PI / 3;
+
+
+// Paint our bitmap.
+FireballSprite.painter = function(bitmap) {
+    var d = FireballSprite.SPARK_R;
+    Utils.resizeBitmap(bitmap, d, d);
+    var c = bitmap.context;
+    c.fillStyle = '#ffffff';
+    c.fillRect(0, 0, d, d);
+};
+
+// Update colors.
+FireballSprite.prototype.setPalette = function(palette) {
+    this.sparktint = palette.c2.i;
+};
+
+// Get current orb coords (relative to our origin).
+FireballSprite.prototype.getOrbCoords = function() {
+    var r = this.orb.y;
+    var a = -this.spinner.rotation;
+    var x = r * Math.sin(a);
+    var y = r * Math.cos(a);
+    return { x: x, y: y };
+};
+
+// Update loop.
+FireballSprite.prototype.update = function() {
+    Phaser.Sprite.prototype.update.call(this);
+    var time = this.game.time.now;
+    if (time > this.sparkTime) {
+        new FireballSprite.Spark(this);
+        this.sparkTime = time + FireballSprite.SPARK_INTERVAL;
+    }
+};
+
+
+
+
+
+
+// Fireball trail.
+FireballSprite.Spark = function(fbsprite) {
+    this.game = fbsprite.game;
+    var p = fbsprite.getOrbCoords();
+    Phaser.Sprite.call(this, this.game, p.x, p.y,
+        fbsprite.bitmap);
+    this.anchor.setTo(0.5);
+    fbsprite.addChild(this);
+    this.rotation = fbsprite.spinner.rotation;
+
+    this.rgb = Color.rgb(this.game.settings.colors.WHITE.i);
+    var rgb = Color.rgb(fbsprite.sparktint);
+    this.game.add.tween(this.rgb).to(rgb,
+        FireballSprite.SPARK_LIFETIME,
+        Phaser.Easing.Quadratic.Out, true,
+        FireballSprite.SPARK_TINT_DELAY);
+
+    this.scale.setTo(FireballSprite.SPARK_STARTING_SCALE);
+    var time = FireballSprite.SPARK_GROWTH_TIME;
+    var scale = FireballSprite.SPARK_GROWTH_SCALE;
+    var t = this.game.add.tween(this.scale).to({ x: scale, y: scale },
+        time, Phaser.Easing.Linear.None, true);
+    var scale = 0.1 + Math.random() * 0.2;
+    var t2 = this.game.add.tween(this.scale).to({ x: scale, y: scale },
+        FireballSprite.SPARK_LIFETIME - time,
+        Phaser.Easing.Linear.None);
+    t.chain(t2);
+
+    var v = FireballSprite.DRIFT_VARIANCE;
+    var dx = Math.random() * v - v / 2;
+    var dy = Math.random() * v - v / 2;
+    var x = this.x + dx;
+    var y = this.y + dy;
+    var a = FireballSprite.ANGLE_VARIANCE;
+    var rotation = this.rotation + Math.random() * a - a / 2;
+    var t = this.game.add.tween(this).to({
+            x: x,
+            y: y,
+            rotation: rotation
+        },
+        FireballSprite.SPARK_LIFETIME,
+        Phaser.Easing.Linear.None, true);
+    t.onComplete.add(function() {
+        this.kill();
+    }, this);
+};
+
+FireballSprite.Spark.prototype = Object.create(Phaser.Sprite.prototype);
+FireballSprite.Spark.prototype.constructor = FireballSprite.Spark;
+
+
+// Update loop.
+FireballSprite.Spark.prototype.update = function() {
+    Phaser.Sprite.prototype.update.call(this);
+    this.tint = (this.rgb.r << 16) +
+        (this.rgb.g << 8) +
+        (this.rgb.b);
+};
---
+++ js/level/obstacle/Fireball.js
@@ -0,0 +1,114 @@
+// Circling fireball trap.
+var Fireball = function(name, x, y,
+    leash, speedRatio, clockwise, startAnglePercentage,
+    returnAnglePercentage) {
+    Obstacle.call(this, name, x, y);
+    this.leash = leash;
+    if (leash && leash != Fireball.DEFAULT_LEASH) {
+        this.radius = leash;
+    } else {
+        this.radius = Fireball.DEFAULT_LEASH;
+    }
+    this.speedRatio = speedRatio != undefined ? speedRatio : 1;
+    this.clockwise = clockwise != undefined ? clockwise : true;
+
+    this.startAnglePercentage = startAnglePercentage;
+    if (startAnglePercentage != undefined) {
+        this.startAngle = 2 * Math.PI * startAnglePercentage;
+    } else {
+        this.startAngle = 0;
+    }
+
+    this.returnAnglePercentage = returnAnglePercentage;
+    if (returnAnglePercentage != undefined) {
+        this.returnAngle = 2 * Math.PI * returnAnglePercentage;
+        this.rotation = this.returnAngle;
+    } else {
+        this.rotation = this.startAngle + 2 * Math.PI;
+    }
+    if (!this.clockwise) {
+        this.rotation = this.rotation - 2 * Math.PI;
+    }
+
+    this.percentage = Math.abs(
+        (this.rotation - this.startAngle) / (2 * Math.PI));
+    this.yoyo = this.returnAngle;
+    this.time = this.percentage * Fireball.TIME / this.speedRatio;
+};
+
+Fireball.TYPE = 'foe-fireball';
+Fireball.prototype = Object.create(Obstacle.prototype);
+Fireball.prototype.constructor = Fireball;
+
+// Set up our factory.
+Obstacle.load.factory[Fireball.TYPE] = Fireball;
+
+// Constants.
+Fireball.HITBOX = 30;
+Fireball.DEFAULT_LEASH = 100;
+Fireball.TIME = 2000;
+
+
+// Draw loop.
+Fireball.prototype.draw = function(tier) {
+    if (this.renderNeeded) {
+        this.game = tier.game;
+        this.renderNeeded = false;
+        this.hitbox = new Hitbox(this.game, tier, this,
+            this.gx, this.gy, Fireball.HITBOX);
+
+        var ip = tier.translateGamePointToInternalPoint(
+            this.x, this.y);
+        var ap = tier.translateInternalPointToAnchorPoint(
+            ip.x, ip.y);
+        this.fireball = new FireballSprite(this, ap.x, ap.y,
+            tier.palette);
+        tier.image.addChild(this.fireball);
+    } else {
+        this.hitbox.updateTier(tier);
+        this.fireball.setPalette(tier.palette);
+    }
+};
+
+// Collision check.
+Fireball.prototype.obstruct = function(avatar, hitbox) {
+    var p = this.fireball.getOrbCoords();
+    var x = this.gx + p.x;
+    var y = this.gy + p.y;
+    var d = Utils.distanceBetweenPoints(avatar.x, avatar.y, x, y);
+    if (d < Fireball.HITBOX) {
+        avatar.smite({ x: x, y: y });
+    }
+    return false;
+};
+
+// Delete ourself.
+Fireball.prototype.delete = function() {
+    if (this.Fireball) {
+        Utils.destroy(this.Fireball);
+        this.Fireball = undefined;
+    }
+    if (this.hitbox) {
+        this.traphitbox.removeCollision();
+        this.traphitbox = undefined;
+    }
+};
+
+// Editor details.
+Fireball.prototype.getDetails = function() {
+    return Obstacle.prototype.getDetails.call(this);
+};
+
+// Write our JSON conversion.
+Fireball.prototype.toJSON = function() {
+    var result = Obstacle.prototype.toJSON.call(this);
+
+    return result;
+};
+
+// Load our JSON representation.
+Fireball.load = function(game, name, json) {
+    return new Fireball(name, json.x, json.y,
+        json.leash, json.speedRatio, json.clockwise,
+        json.startAnglePercentage, json.returnAnglePercentage);
+};
