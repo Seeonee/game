6b016e6947a1587592e9e5fd05f537882d0816a1
Seeonee <gecko4D@yahoo.com>
2017-01-12 17:11:54 -0500

    more_slash_gfx_updates
    Increasingly better looking. Still lacks a lot of oomph and particles.

--- js/gfx/power/SlashEffects.js
+++ js/gfx/power/SlashEffects.js
@@ -1,38 +1,51 @@
 // Slash.
 var SlashEffects = function(slash) {
     this.game = slash.game;
-    var bitmap = this.game.bitmapCache.get(
-        SlashEffects.painter);
-    Phaser.Sprite.call(this, this.game, 0, 0, bitmap);
+
+    this.bitmap = this.createBitmap();
+    Phaser.Sprite.call(this, this.game, 0, 0, this.bitmap);
     this.anchor.setTo(0, 0.5);
-    this.tweens = [];
     this.alpha = 0;
+
+    this.r = SlashEffects.R1;
+    this.update();
+
+    this.tweens = [];
 };
 
 SlashEffects.prototype = Object.create(Phaser.Sprite.prototype);
 SlashEffects.prototype.constructor = SlashEffects;
 
 // Constants.
-SlashEffects.ARC_ALPHA1 = 0.3;
-SlashEffects.ARC_ALPHA2 = 0;
-SlashEffects.FLICKER_TIME = 650; // ms
+SlashEffects.R1 = 0.3;
+SlashEffects.ARC_ALPHA1 = 0.5;
+SlashEffects.ARC_W = 5;
+SlashEffects.FADE_IN = 250; // ms
 
+
+// Make our bitmap.
+SlashEffects.prototype.createBitmap = function() {
+    var r = SlashPower.RADIUS;
+    return this.game.add.bitmapData(r + SlashEffects.ARC_W, 2 * r);
+};
 
 // Paint our bitmap.
-SlashEffects.painter = function(bitmap) {
-    var r = SlashPower.RADIUS;
+SlashEffects.prototype.update = function() {
+    Phaser.Sprite.prototype.update.call(this);
+    if (this._r == this.r) {
+        return;
+    }
+    this._r = this.r;
+    var r = SlashPower.RADIUS * this._r;
     var a = SlashPower.CATCH;
-    var pad = 3;
-    Utils.resizeBitmap(bitmap, r + 2 * pad, 2 * r + 2 * pad);
-    var c = bitmap.context;
-    // c.fillStyle = '#ffffff';
+    var c = this.bitmap.context;
+    c.clearRect(0, 0, this.bitmap.width, this.bitmap.height);
     c.strokeStyle = '#ffffff';
-    c.lineWidth = 2;
+    c.lineWidth = SlashEffects.ARC_W;
     c.beginPath();
-    c.moveTo(0 + pad, r + pad);
-    c.arc(0, r, r, -a, a, false);
-    c.closePath();
+    c.arc(0, SlashPower.RADIUS, r, -a, a, false);
     c.stroke();
+    this.bitmap.dirty = true;
 };
 
 // Stop all tweens.
@@ -45,12 +58,17 @@ SlashEffects.prototype.clearTweens = function() {
 
 // Begin targeting.
 SlashEffects.prototype.arm = function() {
+    this.clearTweens();
+    this.r = SlashEffects.R0;
     var t = this.game.add.tween(this);
-    this.alpha = SlashEffects.ARC_ALPHA1;
-    t.to({ alpha: SlashEffects.ARC_ALPHA2 },
-        SlashEffects.FLICKER_TIME,
-        Phaser.Easing.Bounce.InOut, true,
-        0, Number.POSITIVE_INFINITY, true);
+    t.to({ r: SlashEffects.R1 },
+        SlashEffects.FADE_IN, Phaser.Easing.Cubic.Out, true);
+    this.tweens.push(t);
+
+    this.alpha = 0;
+    var t = this.game.add.tween(this);
+    t.to({ alpha: SlashEffects.ARC_ALPHA1 },
+        SlashEffects.FADE_IN, Phaser.Easing.Cubic.Out, true);
     this.tweens.push(t);
 };
 
@@ -61,6 +79,11 @@ SlashEffects.prototype.slash = function() {
     var t = this.game.add.tween(this);
     t.to({ alpha: 0 }, SlashPower.LOCKOUT,
         Phaser.Easing.Cubic.In, true);
+    this.tweens.push(t);
+
+    var t = this.game.add.tween(this);
+    t.to({ r: 1 }, SlashPower.LOCKOUT,
+        Phaser.Easing.Cubic.Out, true);
     this.tweens.push(t);
 };
 
